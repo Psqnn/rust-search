9cd20ebab5f6150e2395dd05bd4f97035f382ab2
Niko_Verybig_Weed <ppasa5684@gmail.com>
2026-01-20 00:28:30 +0800

    Update lib.rs

--- srс/lib.rs
+++ srс/lib.rs
@@ -1,5 +1,7 @@
 pub mod models;
 pub mod indexer;
+pub mod search_engine;
 
-pub use models::{FileInfo, SearchResult};
+pub use models::{SearchResult, SearchFilter, FileInfo, DatabaseStats};
 pub use indexer::Indexer;
+pub use search_engine::SearchEngine;
